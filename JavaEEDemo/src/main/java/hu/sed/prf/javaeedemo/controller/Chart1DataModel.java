3f250e53eb782f2e46d1f698f93deed574bebe3d
hvik <viki90930@freemail.hu>
2016-05-11 22:26:32 +0200

    Add files via upload

--- JavaEEDemo/src/main/java/hu/sed/prf/javaeedemo/controller/Chart1DataModel.java
+++ JavaEEDemo/src/main/java/hu/sed/prf/javaeedemo/controller/Chart1DataModel.java
@@ -4,6 +4,9 @@ import hu.sed.prf.javaeedemo.controller.AbstractDataModel;
 import hu.sed.prf.javaeedemo.dao.GenericDao;
 import hu.sed.prf.javaeedemo.dao.ProductDao;
 import hu.sed.prf.javaeedemo.entity.Product;
+
+import java.util.List;
+
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -14,16 +17,27 @@ public class Chart1DataModel extends AbstractDataModel<Product, Long> {
 	
 	private static final long serialVersionUID = -7210487593216166015L;
 	
+	private List<Object[]> list;
+	
+	
 	@Inject
 	private ProductDao productDao;
 	@Override
 	protected GenericDao<Product, Long> getEntityDao() {
 		return productDao;
 	}
+	
+	public List<Object[]> getListObject(){
+		return list;
+	}
+	
+	public void setListObject(List<Object[]> list){
+		this.list = list;
+	}
 
     @Override
     public void loader() {
-	setList(productDao.findAllProduct());
+	setListObject(productDao.numberOfProductsByCategories());
     }
 	
 	
--- JavaEEDemo/src/main/java/hu/sed/prf/javaeedemo/controller/Chart2DataModel.java
+++ JavaEEDemo/src/main/java/hu/sed/prf/javaeedemo/controller/Chart2DataModel.java
@@ -1,35 +1,75 @@
 package hu.sed.prf.javaeedemo.controller;
 
+import javax.annotation.PostConstruct;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+ 
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.DateAxis;
+
+import hu.sed.prf.javaeedemo.controller.AbstractDataModel;
+import hu.sed.prf.javaeedemo.dao.GenericDao;
+import hu.sed.prf.javaeedemo.dao.StorageDao;
+import hu.sed.prf.javaeedemo.entity.Storage;
+
+import java.util.List;
+
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import hu.sed.prf.javaeedemo.controller.core.AbstractDataModel;
-import hu.sed.prf.javaeedemo.controller.person.SelectedPatientContainer;
-import hu.sed.prf.javaeedemo.dao.core.GenericDAO;
-import hu.sed.prf.javaeedemo.dao.measurement.WeightDataDAO;
-import hu.sed.prf.javaeedemo.entity.measurement.WeightData;
-
 @Named
 @ViewScoped
-public class Chart2DataModel extends AbstractDataModel<WeightData, Long> {
-	
-	private static final long serialVersionUID = -3446654405173782044L;
-	
+public class Chart2DataModel extends AbstractDataModel<Storage, Long> {
+
+	private static final long serialVersionUID = -7210487593216166015L;
+
+	private List<Object[]> list;
 	@Inject
-	private WeightDataDAO weightDataDAO;
-	
-	@Inject
-	private SelectedPatientContainer selectedPatientContainer;
-	
+	private StorageDao storageDao;
+
 	@Override
-	public void load() {
-		setList(weightDataDAO.findByPatient(selectedPatientContainer.getSelectedPatient()));
+	protected GenericDao<Storage, Long> getEntityDao() {
+		return storageDao;
+	}
+
+	public List<Object[]> getListObject() {
+		return list;
+	}
+
+	public void setListObject(List<Object[]> list) {
+		this.list = list;
 	}
 
 	@Override
-	protected GenericDAO<WeightData, Long> getEntityDao() {
-		return weightDataDAO;
+	public void loader() {
+		setListObject(storageDao.partNumberByStorage());
 	}
+	
+	/*private LineChartModel chartModel;
+	 
+	public void initialize() {
+	  chartModel = new LineChartModel();
+	  
+	  DateAxis dateAxis = new DateAxis();
+	  dateAxis.setTickAngle(-90);
+	  dateAxis.setTickFormat("%Y. %m. %d. %H:%M:%S");
+	  chartModel.getAxes().put(AxisType.X, dateAxis);
+	  
+	  LineChartSeries lineChartSeries = new LineChartSeries();
+	  List<Object[]> storages = storageDao.partNumberByStorage();
+	  for (Object[] o : storages) {
+	   lineChartSeries.set(o[1], (Number) o[0]);
+	  }
+	  chartModel.addSeries(lineChartSeries);
+	}
+	 
+	public LineChartModel getChartModel() {
+	  return chartModel;
+	}*/
 
 }
